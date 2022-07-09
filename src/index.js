c3d698e780d01f2d445efc2db34bf0dd9e28c999
Veanty <plmulticraftpl@gmail.com>
2022-07-09 11:00:37 +0200

    Chore: create webpack structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import { toDo } from "../src/toDoCreate";
+
+const todo1 = new toDo("Cook", "Cook with parents", "29-08-2022", 4);
+
+console.log(todo1);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TODOer",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|ptf|ttf|eot|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
